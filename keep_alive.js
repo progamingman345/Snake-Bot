fa01c3e81d89f3978431fd0ae6c35cb0939e2511
progamingman345 <85048449+progamingman345@users.noreply.github.com>
2021-05-30 14:47:10 +0800

    Delete keep_alive.js

--- keep_alive.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => res.send('Your bot is alive!')); 
-
-app.listen(port, () => console.log(`Your bot is listening to http://localhost:${port}`));
